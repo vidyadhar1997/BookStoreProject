15dbffb40ac4030bf43042ebb1c7be369b78ed88
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-02-28 10:42:20 +0530

    Added:Order summary page and scss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router,  Route, Switch} from 'react-router-dom';
 import CartBooks from './component/CartBooks/CartBooks';
 import Dashboard from './component/Dashbord/DisplayBook';
+import OrderSummary from './component/OrderSummary/OrderSummary';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
@@ -18,6 +19,7 @@ function App() {
             <Route path="/signup" component={Registration} exact={true} />
             <Route path="/cartbooks" component={CartBooks} exact={true} /> 
             <Route path="/home" component={Home} exact={true} />
+            <Route path="/ordersummary" component={OrderSummary} exact={true} />
         </Switch>
         </Router>
       </div>
--- src/component/OrderSummary/OrderSummary.jsx
+++ src/component/OrderSummary/OrderSummary.jsx
@@ -1,7 +1,65 @@
 import React from 'react';
 import '../OrderSummary/OrderSummary.scss'
-export default function OrderSummary(){
-    return(
+import CheckoutImage from "../../assets/orderSummaryimg.jpg"
+import AppBar from '../AppBar/AppBar';
 
-    )
+export default function OrderSummary() {
+    return (
+        <div>
+        <AppBar/>
+        
+        <div className="ordersummaryPage">
+            <div className="summary">
+                <div className="ordersummaryimage">
+                    <img
+                        src={CheckoutImage}
+                        className="ordersummaryimage"
+                        alt="summary img"
+                    />
+                </div>
+                <br />
+                <div className="Comfirmation">
+                    Hurray!!!your order is confirmed
+                    the order id is # save order id for
+                    further communication..
+              </div>
+
+
+                <div className="tablecontainer" >
+                    <br />  <br /> 
+                    <div className="table">
+
+                        <div className="flexSetting">
+                            <div className="Columns">
+                                <div className="headersss"> Email Id</div><br />
+                                <div className="headersss">admin@bookstore.com</div>
+                            </div>
+                            <div className="Columns">
+                                <div className="headersss">Contact us </div> <br />
+                                <div className="headersss">+91 8149713160 </div>
+                            </div>
+                            <div className="Columns">
+                                <div className="headersss">Address</div>
+                                <br />
+                                <div className="headersss">
+                                    42, 14th Main, 15th Cross,Sector 4,opp to BDA complex,
+                                    near Kumarakom restraurant,HSR Layout,Banglore 560034
+                      </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div >
+
+                    <button type="button" className="ContinueShopping">CONTINUE SHOPPING</button>
+                </div>
+            </div>
+            <br />
+            <div>
+
+            </div>
+        </div>
+        </div>
+    );
 }
